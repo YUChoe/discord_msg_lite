427d8cc76680e42694d837e89fbd2a5e36295193
YUChoe@7577 <yonguk.choe@gmail.com>
2019-09-27 20:02:10 +0900

    Fixed typo

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
