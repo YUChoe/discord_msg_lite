6097dd4233a9f5b312f914bc499c6d5dda5048a2
YUChoe@7577 <yonguk.choe@gmail.com>
2019-09-27 19:38:12 +0900

    Reset webhool_url

--- test_discord_msg_lite.py
+++ test_discord_msg_lite.py
@@ -2,6 +2,6 @@ from src.discord_msg_lite import Discord_msg
 import time
 
 def test_send():
-    webhook_url = 'https://discordapp.com/api/webhooks/627020063651790858/8hxP4ZZj-8KMw6jE04VEK5BIfRxpz53VN7wLEMCwCwgHxpZOTR95dPWRjybaNOQzXjAO'
+    webhook_url = ''
     dmsg = Discord_msg(webhook_url)
     assert dmsg.send('test 12345 ㅁㄴㅇㄹ at {}'.format(time.asctime())) == ''
